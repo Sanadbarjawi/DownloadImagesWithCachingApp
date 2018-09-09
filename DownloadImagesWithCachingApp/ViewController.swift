97bcf71c42f3ae7181b0ff8a5abb866fdad442af
Sanad Barjawi <sanadbarjawi@Khalids-iMac.local>
2018-09-09 10:11:01 +0300

    king fisher implemented and used in an extention

--- DownloadImagesWithCachingApp/ViewController.swift
+++ DownloadImagesWithCachingApp/ViewController.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
-
+import Kingfisher
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView1: UIImageView!
+    @IBOutlet weak var imgView2: UIImageView!
+    @IBOutlet weak var imgView3: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imgView1.downloadImage(url: "https://images.pexels.com/photos/912110/pexels-photo-912110.jpeg?auto=compress&cs=tinysrgb&h=650&w=940")
+        imgView2.downloadImage(url: "https://images.pexels.com/photos/912110/pexels-photo-912110.jpeg?auto=compress&cs=tinysrgb&h=650&w=940")
+        imgView3.downloadImage(url: "https://images.pexels.com/photos/912110/pexels-photo-912110.jpeg?auto=compress&cs=tinysrgb&h=650&w=940")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension UIImageView{
+    func downloadImage(url:String){
+        let url = URL(string: url)!
+        self.kf.indicatorType = .activity
+        self.kf.setImage(with: url)
+    }
+}
